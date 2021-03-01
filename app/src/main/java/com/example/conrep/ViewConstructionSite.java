77b4a203038de841f9ac0229f6c012e17aff633a
Ordred <jonathan.albrecht@students.hevs.ch>
2021-03-01 19:53:13 +0100

    View Construction Site layout

---
+++ app/src/main/java/com/example/conrep/ViewConstructionSite.java
@@ -0,0 +1,16 @@
+package com.example.conrep;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class ViewConstructionSite extends AppCompatActivity {
+
+
+}
