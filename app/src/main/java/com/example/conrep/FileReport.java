16bf0d7a41d1d3fc58d8254de7842603c439bebe
HutterSven <64769368+HutterSven@users.noreply.github.com>
2021-03-02 23:06:46 +0100

    File Report, Add task, Task List and View Task UIs added

---
+++ app/src/main/java/com/example/conrep/FileReport.java
@@ -0,0 +1,14 @@
+package com.example.conrep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FileReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_report);
+    }
+}
