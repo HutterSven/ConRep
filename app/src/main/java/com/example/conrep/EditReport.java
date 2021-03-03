c9c5ad7a33aa4d079a532cc3947bbfa41fabb9af
HutterSven <64769368+HutterSven@users.noreply.github.com>
2021-03-03 15:06:39 +0100

    fixed View Task and added map, edit task, view report and edit report

---
+++ app/src/main/java/com/example/conrep/EditReport.java
@@ -0,0 +1,14 @@
+package com.example.conrep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_report);
+    }
+}
