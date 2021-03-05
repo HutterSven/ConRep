d83bf50987588d45ffaff96e6775cb6e839d7a7d
Ordred <jonathan.albrecht@students.hevs.ch>
2021-03-05 13:25:27 +0100

    edit and add corrected

--- app/src/main/java/com/example/conrep/AddConstructionSite.java
+++ app/src/main/java/com/example/conrep/AddConstructionSite.java
@@ -1,21 +1,14 @@
 package com.example.conrep;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
 
-public class AddConstructionSite extends WearableActivity {
-
-    private TextView mTextView;
+public class AddConstructionSite extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_construction_site);
-
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
     }
 }
