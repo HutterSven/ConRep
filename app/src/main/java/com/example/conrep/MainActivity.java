cb645638a758cea82d5fcb52c7fabef449642259
Ordred <jonathan.albrecht@students.hevs.ch>
2021-03-15 15:48:58 +0100

    first onclick listener

--- app/src/main/java/com/example/conrep/MainActivity.java
+++ app/src/main/java/com/example/conrep/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.conrep;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button addSite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addSite = findViewById(R.id.button8);
+        addSite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddSite();
+            }
+
+
+
+        });
     }
+
+
+    private void openAddSite() {
+        Intent intent = new Intent(this, AddConstructionSite.class);
+        startActivity(intent);
+    }
+
+
 }
