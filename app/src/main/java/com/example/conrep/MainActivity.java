bfc69343503097e49744aa99a630636c94cd82d3
HutterSven <64769368+HutterSven@users.noreply.github.com>
2021-03-15 16:36:52 +0100

    added actionbar

--- app/src/main/java/com/example/conrep/MainActivity.java
+++ app/src/main/java/com/example/conrep/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.conrep;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Menu;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button addSite;
+    private Button addSite, settings, about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +23,42 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 openAddSite();
             }
-
-
-
         });
+
+//        settings = findViewById(R.id.settings);
+//        settings.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openSettings();
+//            }
+//        });
+//
+//        about = findViewById(R.id.about);
+//        about.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openAbout();
+//            }
+//        });
     }
 
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     private void openAddSite() {
         Intent intent = new Intent(this, AddConstructionSite.class);
+        startActivity(intent);
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+
+    private void openAbout() {
+        Intent intent = new Intent(this, About.class);
         startActivity(intent);
     }
 
