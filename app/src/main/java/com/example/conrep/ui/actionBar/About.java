49e31b7d25f0d99c38fb89b00763cda404b755a8
HutterSven <64769368+HutterSven@users.noreply.github.com>
2021-04-02 18:24:43 +0200

    chanched settings

--- app/src/main/java/com/example/conrep/ui/actionBar/About.java
+++ app/src/main/java/com/example/conrep/ui/actionBar/About.java
@@ -1,16 +1,39 @@
 package com.example.conrep.ui.actionBar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.conrep.R;
+import com.example.conrep.ui.BaseActivity;
+import com.example.conrep.ui.MainActivity;
 
-public class About extends AppCompatActivity {
+public class About extends BaseActivity {
+
+    private Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        getLayoutInflater().inflate(R.layout.activity_about, frameLayout);
+
+        initiateView();
+
+    }
+
+    private void initiateView() {
+        backBtn = findViewById(R.id.btnBackAbout);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                back();
+            }
+        });
+    }
+
+    private void back() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
